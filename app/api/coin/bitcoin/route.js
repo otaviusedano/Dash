b42f87dd5ef21abfb0ab23c7770165d33fa73fd3
otaviusedano <sedanootavio@gmail.com>
2023-06-19 20:41:04 -0300

    fix: deployment

--- app/api/coin/bitcoin/route.js
+++ app/api/coin/bitcoin/route.js
@@ -2,11 +2,6 @@ import bitcoin from "../../../../data/apiBitcoin.json"
 import { NextResponse } from "next/server"
 
 
-export const config = {
-	runtime: "edge",
-	regions: ["sfo1", "iad1"]
-}
-
 export function GET() {
 
 	return NextResponse.json(bitcoin)
