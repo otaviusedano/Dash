a145e410a3030f80e46b62adc5baea23f82b6958
otaviusedano <sedanootavio@gmail.com>
2023-06-19 20:44:11 -0300

    fix: deployment

--- app/api/coin/bitcoin/route.js
+++ app/api/coin/bitcoin/route.js
@@ -2,7 +2,12 @@ import bitcoin from "../../../../data/apiBitcoin.json"
 import { NextResponse } from "next/server"
 
 
+export const config = {
+	runtime: "edge",
+	regions: ["sfo1", "iad1"]
+}
+
 export function GET() {
 
-	return NextResponse.json(bitcoin)
+	return NextResponse.json(bitcoin, { nextConfig: { basePath: process.env.VERCEL_URL}, url: process.env.VERCEL_URL })
 }
