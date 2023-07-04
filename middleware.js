de7cf0ac5f11294d4eac2e254e7e9b0ba4887096
otaviusedano <sedanootavio@gmail.com>
2023-07-04 15:05:33 -0300

    change: middleware

--- middleware.js
+++ middleware.js
@@ -1,17 +1,22 @@
-import { withAuth } from 'next-auth/middleware'
+import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
 
+export async function middleware(req, _) {
+  if (req.nextUrl.pathname !== "/login" || "signup") {
+    const session = await getToken({
+      req,
+      secret: process.env.NEXTAUTH_SECRET,
+    })
 
-export default withAuth(
-  function middleware() {},
-  {
-    callbacks: {
-      authorized: (params) => {
-        let { token } = params
-        return !!token
-      }
+    if (!session) {
+      const url = req.nextUrl.clone()
+
+      url.pathname = "/login"
+
+      return NextResponse.redirect(url)
     }
   }
-)
+}
 
 export const config = {
   matcher: ["/((?!signup|api|login|_next/static|favicon.ico).*)"],
