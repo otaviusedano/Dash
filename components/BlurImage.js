08ac21093adaae1e48a11611220bf49b0346726e
otaviusedano <sedanootavio@gmail.com>
2023-07-04 17:29:34 -0300

    fix: image

--- components/BlurImage.js
+++ components/BlurImage.js
@@ -7,21 +7,23 @@ const BlurImage = ({ src, ...props }) => {
   const [isLoading, setLoading] = useState(true);
 
   return (
-    <Image
-      {...props}
-      src={src}
-      layout="fill"
-      objectFit="cover"
-      className={`
-          w-full duration-700 ease-in-out
-          object-cover desktop:object-left-top
-          ${
-            isLoading
-              ? "scale-105 blur-2xl grayscale"
-              : "scale-100 blur-0 grayscale-0"
-          })`}
-      onLoadingComplete={() => setLoading(false)}
-    />
+    <div>
+      <Image
+        {...props}
+        src={src}
+        layout="fill"
+        objectFit="cover"
+        className={`
+        w-full duration-700 ease-in-out
+        object-cover desktop:object-left-top
+        ${
+          isLoading
+          ? "scale-105 blur-2xl grayscale"
+          : "scale-100 blur-0 grayscale-0"
+        })`}
+        onLoadingComplete={() => setLoading(false)}
+      />
+    </div>
   );
 }
 
